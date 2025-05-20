f34235196c838bc963c892ad3afdde8fdc4eeef6
ophelia-ups <victoriafermin104@gmail.com>
2025-05-19 19:14:05 -0500

    configuracion de main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,33 @@
-trust-policy.json
+provider "aws" {
+  region = "us-east-1-lim-1a"
+}
+
+resource "aws_iam_role" "my_role" {
+  name               = "my-example-role"
+  assume_role_policy = data.aws_iam_policy_document.trust_policy.json
+}
+
+resource "aws_iam_policy" "my_policy" {
+  name        = "my-example-policy"
+  description = "My example IAM policy"
+  policy      = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "example_attach" {
+  role       = aws_iam_role.my_role.name
+  policy_arn = aws_iam_policy.my_policy.arn
+}
+
+data "aws_iam_policy_document" "trust_policy" {
+  statement {
+    actions   = ["sts:AssumeRole"]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    actions   = ["s3:ListBucket", "s3:GetObject"]
+    resources = ["arn:aws:s3:::my-bucket", "arn:aws:s3:::my-bucket/*"]
+  }
+}
